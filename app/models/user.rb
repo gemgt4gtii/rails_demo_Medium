c594e3ea073355633fce863267e8de28966ecd51
gemgt4gtii <z123456618@hotmail.com>
2020-12-12 01:08:51 +0800

    Photo upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :stories
-  validates :username, presence: true, uniqueness: true       
+  validates :username, presence: true, uniqueness: true 
+
+  has_one_attached :avatar    
 end
