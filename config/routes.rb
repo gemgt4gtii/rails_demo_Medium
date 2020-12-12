3e1a22fdae3dff4fcc2e0fb7dc7868274166525d
gemgt4gtii <z123456618@hotmail.com>
2020-12-12 17:30:21 +0800

    Home_Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :stories
 
-
+  # /@使用者名稱/文章標題
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+  get '@:username', to: 'pages#user', as: 'user_page'
 
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
