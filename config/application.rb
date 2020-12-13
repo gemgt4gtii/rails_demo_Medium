5e13eba4787d036f5ac94972f230b632903c4689
gemgt4gtii <z123456618@hotmail.com>
2020-12-13 19:08:20 +0800

    Determine user login status

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module DemoMedium
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Taipei'
+
+    # 生成時，忽略以下文件
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
